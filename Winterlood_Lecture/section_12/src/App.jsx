4a93cd58249c3fb9a00fee2819bd6b2e54f2f761
kuru2141 <kuru2141@naver.com>
2025-05-06 22:27:55 +0900

    FEAT: 학습 9일차

---
+++ Winterlood_Lecture/section_12/src/App.jsx
@@ -0,0 +1,43 @@
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+
+// 1. "/": 모든 일기를 조회하는 Home 페이지
+// 2. "/new": 새로운 일기를 작성하는 New 펭지
+// 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
+function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+
+        {/* 기존의 a 태그는 CSR 방식으로 페이지 이동 불가 */}
+        {/* <a href="/">Home</a>
+        <a href="/new">New</a>
+        <a href="/diary">Diary</a> */}
+      </div>
+
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Winterlood_Lecture/section_12/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useSearchParams } from "react-router-dom";
+
+const Home = () => {
+  // // query string을 쓰고 싶은 경우
+  // const [params, setParams] = useSearchParams();
+  // console.log(params.get("value"));
+
+  return <div>Home</div>;
+};
+
+export default Home;
